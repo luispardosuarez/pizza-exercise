71c98d1dafadb99dcad1001ee45eaf7a39ea7cb4
Borja <elbarmandev@gmail.com>
2023-10-30 09:52:46 +0100

    level 2 finish

--- pizzav2.js
+++ pizzav2.js
@@ -1,24 +1,33 @@
-import promptSync from "prompt-sync";
-
-import { elaborar } from "./pizzaapp.js";
-
 const prompt = promptSync();
 
-const result = prompt("Pideme tu pizza ");
+
+import promptSync from "prompt-sync";
+
+import { elaborarMontaniesa, elaborarLaVeroQueso, precalentar, repartir, cocinarPizza } from "./pizzaapp.js";
+
+function app(){
+
+    const result = prompt("Pideme tu pizza ");
 
 
+}
 console.log("Bienvenido a Pizzas Pardo ¿Qué desea?");
-const opcion = prompt("Elige una pizza :D");
 
 console.log("Opción 1 - Pizza La montañesa ");
+console.log("Opción 2 - Pizza La Vero Queso ");
+
+const opcion = prompt("Elige una pizza :D ");
 
 
 let pizzaElegida;
 
 switch (opcion) {
     case '1':
-        pizzaElegida = elaborar();
+        pizzaElegida = precalentar(); elaborarMontaniesa();  cocinarPizza(2);  repartir();
         break;
+
+    case '2':
+        pizzaElegida = precalentar(); elaborarLaVeroQueso();  cocinarPizza(2);  repartir();
 
     default:
         console.log("Error vuelve a seleccionar la pizza ");
